8d74f1a14dd85f06786719cfb565886f22ae0301
Jack Lansing <jack.lansing@live.com>
2020-02-21 11:43:44 -0500

    added getItems function and createItems function

--- src/api.js
+++ src/api.js
@@ -2,10 +2,23 @@
 const BASE_URL = 'https://thinkful-list-api.herokuapp.com/JackAndrew';
 
 function getItems() {
-    
-  return Promise.resolve('A successful response!');
+  return fetch(`${BASE_URL}/items`);
+}
+
+function createItem(name) {
+  const newItem = JSON.stringify({
+    name: name
+  });
+  
+  return fetch(`${BASE_URL}/items`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: newItem,
+  });
+
 }
 
 export default {
-  getItems
+  getItems,
+  createItem,
 };
--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ import './index.css';
 import shoppingList from './shopping-list';
 
 const main = function () {
-  api.getItems()
-    .then(res => console.log(res));
-
-
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
